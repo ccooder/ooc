78dc741331a75b05ad2a0328c59257fb5c912c83
Fenglu Niu <niufenglu@gmail.com>
2023-05-31 23:07:26 +0800

    定义抽象集合和抽象对象，及其集合的实现

---
+++ Set.c
@@ -0,0 +1,74 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "new.h"
+#include "Set.h"
+#include "Object.h"
+
+const void *Set;
+const void *Object;
+
+#if !defined MANY || MANY < 1
+#define    MANY    10
+#endif
+
+static int heap[MANY];
+
+void *new(const void *type, ...) {
+  int *p;
+  for (p = heap + 1; p < heap + MANY; p++) {
+    if (!*p) {
+      break;
+    }
+  }
+  *p = MANY;
+  return p;
+}
+
+void delete(void *_item) {
+  int *item = _item;
+  if (item) {
+    assert(item > heap && item < heap + MANY);
+    *item = 0;
+  }
+}
+
+void *add(void *_set, const void *_element) {
+  int *set = _set;
+  const int *element = _element;
+  assert(set > heap && set < heap + MANY);
+  assert(*set == MANY);
+  assert(element > heap && element < heap + MANY);
+  if (*element == MANY) {
+    *(int *)element = set - heap;
+  } else {
+    assert(*element == set - heap);
+  }
+  return (void *)element;
+}
+
+void *find(const void *_set, const void *_element) {
+  const int *set = _set;
+  const int *element = _element;
+  assert(set > heap && set < heap + MANY);
+  assert(element > heap && element < heap + MANY);
+  assert(*set == MANY);
+  assert(*element);
+  return (void *) (*element == set - heap ? element : 0);
+}
+
+int contains(const void *_set, const void *_element) {
+  return find(_set, _element) != 0;
+}
+
+void *drop(void *_set, const void *_element) {
+  int *element = find(_set, _element);
+
+  if (element)
+    *element = MANY;
+  return element;
+}
+
+int differ(const void *a, const void *b) {
+  return a != b;
+}
---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include "new.h"
+#include "Object.h"
+#include "Set.h"
+
+int main() {
+  void *s = new(Set);
+  void *s1 = new(Set);
+  void *a = add(s, new(Object));
+  void *b = add(s1, new(Object));
+  void *c = add(s1, a);
+  printf("c=%d\n", *(int *)c);
+  printf("b=%d\n", *(int *)b);
+  printf("a=%d\n", *(int *)a);
+
+  if (contains(s, a) && contains(s, b))
+    puts("ok");
+
+  if (contains(s, c))
+    puts("contains?");
+
+  if (contains(s, a))
+    puts("contains");
+
+  if (differ(a, add(s, a)))
+    puts("differ?");
+
+  if (contains(s, drop(s, a)))
+    puts("drop?");
+
+  delete(drop(s, b));
+  delete(drop(s, c));
+
+  return 0;
+}
+
